f6f1268c2cfbce7f30bd8f6e0e9bef9950412159
chaurwu <wu.chaur@gmail.com>
2016-01-10 00:03:58 -0800

    update to work with changes in spickspan

--- rest-service.go
+++ rest-service.go
@@ -5,13 +5,24 @@ import (
 
 	"github.com/essentier/gopencils"
 	"github.com/essentier/spickspan"
+	"github.com/essentier/spickspan/config"
 	"github.com/essentier/spickspan/model"
 )
 
-var provider = spickspan.GetNomockProvider()
+var provider model.Provider
 
 func init() {
-	err := spickspan.BuildAll()
+	config, err := config.GetConfig()
+	if err != nil {
+		panic("Failed to find and parse spickspan.json. The error is " + err.Error())
+	}
+
+	provider, err = spickspan.GetNomockProvider(config)
+	if err != nil {
+		panic("Failed to get nomock provider. The error is " + err.Error())
+	}
+
+	err = spickspan.BuildAllInConfig(config)
 	if err != nil {
 		panic("Failed to build projects. The error is " + err.Error())
 	}
